862f8a0856fe8388d956a4ce07f989b84e2785cf
bobardou <jeronimoevilar@outlook.com>
2023-12-19 16:52:37 -0300

    fix

--- client/.next/static/chunks/webpack.js
+++ client/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a3980c72a3f198a9"; }
+/******/ 		__webpack_require__.h = function() { return "72ed2f7e16ba4f20"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
